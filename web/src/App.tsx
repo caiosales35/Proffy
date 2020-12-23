2114611552474dcdb4f5f057aab17720bf8e84e3
Caio Cesar de Morais Sales <caiosales35@gmail.com>
2020-12-23 01:06:35 -0300

    Pagina landingpage

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+import Landing from "./pages/Landing";
+
 import "./assets/styles/global.css";
 
 function App() {
-  return <h1>Ola Mundo!!!</h1>;
+  return <Landing />;
 }
 
 export default App;
