789a8bb9aeaa61bd9811c5d1ec7f67f3459ff64c
Caio Cesar de Morais Sales <caiosales35@gmail.com>
2020-12-25 16:53:27 -0300

    Criando rota de listagem de aulas

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,6 +4,7 @@ import ClassesControler from "./controllers/ClassesController";
 const routes = express.Router();
 const classesControlers = new ClassesControler();
 
+routes.get("/classes", classesControlers.index);
 routes.post("/classes", classesControlers.create);
 
 export default routes;
