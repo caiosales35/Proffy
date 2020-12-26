ae707f82d9a75aacb5c979fb80c1284e8faa0706
Caio Cesar de Morais Sales <caiosales35@gmail.com>
2020-12-25 23:01:03 -0300

    Componentes de textarea e select

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import Input from "../../components/Input";
 import PageHeader from "../../components/PageHeader";
 
 import "./styles.css";
+import warningIcon from "../../assets/images/icons/warning.svg";
+import Textarea from "../../components/Textarea";
+import Select from "../../components/Select";
 
 function TeacherForm() {
   return (
@@ -14,19 +18,32 @@ function TeacherForm() {
       <main>
         <fieldset>
           <legend>Seus dados</legend>
-          <div className="input-block">
-            <label htmlFor="name">Nome completo</label>
-            <input type="text" id="name" />
-          </div>
-          <div className="input-block">
-            <label htmlFor="avatar">Avatar</label>
-            <input type="text" id="avatar" />
-          </div>
-          <div className="input-block">
-            <label htmlFor="whatsapp">WhatsApp</label>
-            <input type="text" id="whatsapp" />
-          </div>
+          <Input name="name" label="Nome completo" />
+          <Input name="avatar" label="Avatar" />
+          <Input name="whatsapp" label="WhatsApp" />
+          <Textarea name="bio" label="Biografia" />
         </fieldset>
+        <fieldset>
+          <legend>Sobre a aula</legend>
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: "Artes", label: "Artes" },
+              { value: "Matemática", label: "Matemática" },
+              { value: "Ingles", label: "Ingles" },
+            ]}
+          />
+          <Input name="cost" label="Custo da sua hora por aula" />
+        </fieldset>
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso importante" />
+            Importante! <br />
+            Preencha todos os dados
+          </p>
+          <button type="button">Salvar cadastro</button>
+        </footer>
       </main>
     </div>
   );
